b87a3e48fd0c288012141bd3ee5ffc2496d80ff5
Bardia Barahman <bardiabarahman@Bardias-MacBook-Pro.local>
2022-11-14 14:29:15 -0800

    initial commit for proj

---
+++ README.md
@@ -0,0 +1,84 @@
+# Spotify Rate Limited Ingestion 🎵✨🎶
+
+## Objective:
+Scrape all artists from the Spotify API as quickly as possible, while also designing
+around Spotify's rate limit in order to compute as fast as possible.
+
+## How to run: 
+* set `CLIENT_ID` and `CLIENT_SECRET` from spotify developer account in .env file
+* Activate virtual environment `source venv/bin/activate`
+* Install package dependencies `pip install -r requirements.txt `
+
+## Spotify API Notes:
+* Spotify supports multiple authorization flows, and since this is a script that doesn't
+need to communicate with end users, I chose to use the Client Credentials Flow. Another
+advantage of this is that it allows us to obtain a higher rate limit compared to the 
+Authorization Code flow.
+* Since Spotify's rate limit is calculated based on the number of requests made in a rolling 30 
+second window and they don't disclose how many requests are allowed in that window, I
+figured we would at least need a backoff-retry strategy coupled with sleeping the script for 
+a small period of time to provide some buffer before hitting a 429 error.
+
+## Functions 
+### get_auth_token
+Grabs a Spotify access token with the client secret and client id which are both stored as
+environment variables. Retries on failure.
+
+### get_auth_header
+Header variables for Spotify API requests
+
+### get_data_from_spotify_api
+Calls Spotify API and raises exceptions if our access token expired or we've hit the rate limit. 
+Increments our global counter `requests_sent` and returns json data from endpoint. 
+Retries on failure. 
+
+### get_several_artists
+Request data for 50 artists at a time from one endpoint call. If we haven't seen this artist yet,
+then we add it to our dictionary storing all artists and print data for each artist.
+
+### get_related_artists
+Used to explore deeper from the artists we've already seen. Grabs 20 related artists, and if we
+haven't explored from these artists already, adds them to our set `artist_ids_to_visit`. If we
+haven't seen one of these related artists, add them to our dictionary and print data for each
+new artist. 
+
+### get_featured_playlists
+Grabs playlist ids of top featured playlists from spotify.
+
+### get_artist_ids_from_playlists
+For each playlist id, grabs all tracks in that playlist. From that data we then extract
+artist ids from each playlist. Returns a list of unique artist ids from playlists. 
+
+## Script Overview
+* There are three things we keep track of globally: 
+  * `requests_sent` int - how many requests we've sent to Spotify API
+  * `artists` dict - maps `artist_id` to data about that artist
+  * `artist_ids_to_visit` set - keeps track of which artist ids we have yet to explore from
+* To start, we used `get_featured_playlists` to get the Spotify's featured playlists
+and then from there we grabbed artists ids from tracks in those playlists
+* These initial ids are what we use to start the scraping process. However, we still need some more
+info about each artist like genre, popularity, etc. so we used `get_several_artists` to batch 
+get this artist info with as few requests as possible.
+* With some IDs to start exploring, from each ID, we call `get_related_artists` which prints data
+for each unique new artist we haven't seen before, and stores info on ones we have yet to explore. We
+keep looping as long as we have artists we haven't explored from. 
+* In order to not hit the rate limit, we use `requests_sent` to sleep the script for 10 seconds 
+for every 500 requests we send. 
+  * I did a few rounds of estimation to come up with this number, and chose a more conservative sleep
+  time to avoid hitting a 429 error.
+* We catch two exceptions: 
+  * If we hit the rate limit, we used a backoff-retry strategy where we wait the amount of seconds
+  specified by the retry-after header variable sent in the 429 response. 
+  * If our access token expires, we set a new one. 
+
+
+## Things to improve: 
+* Investigate if using Spotipy, a python client for Spotify's api, makes our script cleaner. I
+didn't use it at first in case I needed data the client could not provide. 
+* With more time, I would expand on the backoff-retry logic and use a token bucket algorithm to 
+monitor outgoing requests to ensure we don't violate a threshold. Since spotify doesn't provide an
+exact number of requests allowed per 30-second window, additional experimentation is required in order
+to estimate this number. Due to the limitations imposed on running more experiments from
+hitting 429 errors and time constraints, I didn't pursue this option since without
+an accurate estimation of what our rate limit is, we wouldn't maximize efficiency from using
+the token bucket algorithm. 
---
+++ main.py
@@ -0,0 +1,189 @@
+import time
+import json
+from datetime import datetime
+from typing import List
+
+import requests
+from retry import retry
+from decouple import config
+from base64 import b64encode
+
+fmt = "%Y-%m-d %H:%M:%S"
+
+SPOTIFY_CLIENT_ID = config('CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = config('CLIENT_SECRET')
+
+AUTH_TOKEN = None
+
+requests_sent = 0
+artists = dict()
+artist_ids_to_visit = set()
+
+
+class SpotifyRateLimitError(Exception):
+    pass
+
+
+class SpotifyAccessTokenExpired(Exception):
+    pass
+
+
+@retry(delay=5, tries=2)
+def get_auth_token():
+    client_str = f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}".encode('ascii')
+    client_str = b64encode(client_str)
+    client_str = client_str.decode('ascii')
+    headers = {
+        'Authorization': f"Basic {client_str}"
+    }
+    data = {
+        "grant_type": "client_credentials",
+    }
+    req = requests.post(
+        "https://accounts.spotify.com/api/token",
+        headers=headers,
+        data=data
+    )
+    global requests_sent
+    requests_sent += 1
+    content = req.content.decode('ascii')
+    json_data = json.loads(content)
+    access_token = json_data['access_token']
+    return access_token
+
+
+def get_auth_header():
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+    global AUTH_TOKEN
+    if not AUTH_TOKEN:
+        AUTH_TOKEN = get_auth_token()
+    headers["Authorization"] = f"Bearer {AUTH_TOKEN}"
+
+    return headers
+
+
+@retry(delay=5, tries=2)
+def get_data_from_spotify_api(url):
+    headers = get_auth_header()
+
+    req = requests.get(url, headers=headers)
+    global requests_sent
+    requests_sent += 1
+    if req.status_code == 429:
+        wait = req.headers['retry-after']
+        raise SpotifyRateLimitError(f"Rate limit hit, retry-after:{wait}")
+    if req.status_code == 401:
+        raise SpotifyAccessTokenExpired(f"Refreshing access token")
+
+    json_data = json.loads(req.content)
+    return json_data
+
+
+def get_several_artists(a_ids):
+    artist_ids_str = ','.join(a_ids)
+    # ids must be value separated string ex: 434,234,243,423
+    url = f'https://api.spotify.com/v1/artists/?ids={artist_ids_str}'
+    json_data = get_data_from_spotify_api(url)
+
+    global artists
+    for artist in json_data['artists']:
+        a_id = artist.get('id')
+        if a_id not in artists:
+            artists[a_id] = {
+                "id": artist.get('id'),
+                "name": artist.get('name'),
+                "genres": artist.get('genres'),
+                "popularity": artist.get('popularity'),
+            }
+            print(artists[a_id])
+
+
+def get_related_artists(artist_id: str):
+    # Note - this returns max 20 artists at a time
+    url = f'https://api.spotify.com/v1/artists/{artist_id}/related-artists'
+    json_data = get_data_from_spotify_api(url)
+
+    global artists
+    for artist in json_data['artists']:
+        a_id = artist.get('id')
+        artist_ids_to_visit.add(a_id)
+
+        if a_id not in artists:
+            artists[a_id] = {
+                "id": artist.get('id'),
+                "name": artist.get('name'),
+                "genres": artist.get('genres'),
+                "popularity": artist.get('popularity'),
+            }
+            print(artists[a_id])
+
+
+def get_featured_playlists():
+    url = f'https://api.spotify.com/v1/browse/featured-playlists/?limit=50'
+    json_data = get_data_from_spotify_api(url)
+
+    playlists = json_data.get('playlists').get('items')
+    pl_ids = set()
+    for p in playlists:
+        playlist_id = p.get('id')
+        pl_ids.add(playlist_id)
+
+    return pl_ids
+
+
+def get_artist_ids_from_playlists(pl_ids) -> List[str]:
+    a_ids = set()
+    for p_id in pl_ids:
+        url = f'https://api.spotify.com/v1/playlists/{p_id}/tracks'
+        json_data = get_data_from_spotify_api(url)
+        if json_data:
+            playlist_data = json_data.get('items')
+            for tr in playlist_data:
+                track = tr.get('track')
+                if track:
+                    track_artists = track.get('artists')
+                    for artist in track_artists:
+                        artist_id = artist.get('id')
+                        a_ids.add(artist_id)
+
+    return list(a_ids)
+
+
+if __name__ == '__main__':
+    start = time.strftime(fmt)
+
+    playlist_ids = get_featured_playlists()
+    initial_ids = get_artist_ids_from_playlists(pl_ids=list(playlist_ids))
+
+    print(f"{len(initial_ids)} initial artists to start exploring from")
+    artist_ids_to_visit.update(initial_ids)
+
+    while True:
+        try:
+            while initial_ids:
+                get_several_artists(a_ids=initial_ids[:50])
+                initial_ids = initial_ids[50:]
+
+            while artist_ids_to_visit:
+                if requests_sent % 1500 == 0:
+                    current_time = time.strftime(fmt)
+                    print("~~ Delaying 30 seconds ~~")
+                    print(datetime.strptime(current_time, fmt)
+                          - datetime.strptime(start, fmt), f'time elapsed')
+                    time.sleep(30)
+
+                next_id = artist_ids_to_visit.pop()
+                get_related_artists(artist_id=next_id)
+                print(f"Requests sent: {requests_sent}")
+                print(f'Artist total - ', len(artists))
+
+        except SpotifyRateLimitError as e:
+            wait_time = e.args[0].split(':')[1]
+            print(f"Spotify Rate Limit hit, sleeping for {wait_time} seconds")
+            time.sleep(int(wait_time))
+        except SpotifyAccessTokenExpired:
+            print("Refreshing access token")
+            AUTH_TOKEN = get_auth_token()
